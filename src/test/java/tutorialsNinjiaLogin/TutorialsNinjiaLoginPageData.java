a10de5bc2daef2e5723258c4cc0a27344a4238f5
toan1231 <chauthai2003@gmail.com>
2023-01-02 22:27:26 -0500

    firstand secondhjj

--- src/test/java/tutorialsNinjiaLogin/TutorialsNinjiaLoginPageData.java
+++ src/test/java/tutorialsNinjiaLogin/TutorialsNinjiaLoginPageData.java
@@ -6,9 +6,9 @@ public class TutorialsNinjiaLoginPageData {
     @DataProvider(name = "regression")
     public Object[][] method(){
         return new Object[][]{
-                {"Chau", "Thai", "66790oo909om997@gmail.com", "1234967", "Kiwismart1", "Kiwismart1"},
+                {"Chau", "Thai", "66790ooopop909om997@gmail.com", "1234967", "Kiwismart1", "Kiwismart1"},
 
-                {"binh","Thai","96677oo909909m990@gmail.com","1234567","Kiwismart1","Kiwismart1"}};
+                {"binh","Thai","96677oo909opop909m990@gmail.com","1234567","Kiwismart1","Kiwismart1"}};
         }
 
 }
